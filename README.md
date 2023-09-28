bbd6e53b515c14224ca057ed14ccfd2f8e422bc2
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2023-09-28 11:47:51 +0900

    Update init.rb and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# redmine_amzn_alb_authn
+
+Redmine plugin to use Amazon ALB for user authentication.
+
+## Installation
+
+Clone this repository to the Redmine plugins directory.
+
+    $ git clone https://github.com/arkedge/redmine_amzn_alb_authn path/to/redmine/plugins/redmine_amzn_alb_authn
--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Redmine::Plugin.register :redmine_amzn_alb_authn do
-  name 'Redmine Amzn Alb Authn plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Amazon ALB authentication plugin'
+  author 'Takahiro Miyoshi'
+  description 'Use Amazon ALB for user authentication'
+  version '0.1.0'
+  url 'https://github.com/arkedge/redmine_amzn_alb_auth'
+  author_url 'https://github.com/sankichi92'
 end
