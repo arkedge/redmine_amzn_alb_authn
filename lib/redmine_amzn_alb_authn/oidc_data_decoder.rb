82aa34ab660e91491c9c0e05b3ca500e501a5363
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2023-09-30 23:45:27 +0900

    Use class attribute for OIDCDataDecoder.key_cache

--- lib/redmine_amzn_alb_authn/oidc_data_decoder.rb
+++ lib/redmine_amzn_alb_authn/oidc_data_decoder.rb
@@ -3,24 +3,23 @@
 require 'net/http'
 
 require 'active_support'
+require 'active_support/core_ext/class/attribute'
 require 'active_support/core_ext/numeric/time'
 require 'jwt'
 
 module RedmineAmznALBAuthn
   # Verifies and decodes the JWT from the X-Amzn-Oidc-Data header sent by ALB.
   class OIDCDataDecoder
+    class_attribute :key_cache, default: ActiveSupport::Cache::MemoryStore.new(expires_in: 1.hour)
+
     def initialize(key_endpoint:, iss: nil)
       @key_endpoint = key_endpoint
       @iss = iss
     end
 
-    def self.key_cache
-      @key_cache ||= ActiveSupport::Cache::MemoryStore.new(expires_in: 1.hour)
-    end
-
     def verify_and_decode!(oidc_data)
       JWT.decode(oidc_data, nil, true, algorithm: 'ES256', iss: @iss, verify_iss: true) do |header|
-        key_str = self.class.key_cache.fetch(header.fetch('kid')) do
+        key_str = key_cache.fetch(header.fetch('kid')) do
           key_uri = URI.join(@key_endpoint, header['kid'])
           key_response = Net::HTTP.get_response(key_uri)
           key_response.value
