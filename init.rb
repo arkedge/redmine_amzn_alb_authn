6183f55ff2a37b30022b325db6ece4cdedaf308f
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2023-10-04 14:22:59 +0900

    v0.1.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_amzn_alb_authn do
   name 'Amazon ALB authentication plugin'
   author 'Takahiro Miyoshi'
   description 'Use Amazon ALB for user authentication'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/arkedge/redmine_amzn_alb_auth'
   author_url 'https://github.com/sankichi92'
 end
