a86f35afc771b927400c50f52af4bbf9ae2b3aaa
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2023-10-04 12:57:45 +0900

    Rename RedmineAmznALBAuthn => RedmineAmznAlbAuthn
    To follow the Zeitwerk rule

--- lib/redmine_amzn_alb_authn.rb
+++ lib/redmine_amzn_alb_authn.rb
@@ -5,6 +5,6 @@ require 'active_support/core_ext/module/attribute_accessors'
 require_relative 'redmine_amzn_alb_authn/application_controller_patch'
 
 # Plugin namespace and configuration store.
-module RedmineAmznALBAuthn
+module RedmineAmznAlbAuthn
   mattr_accessor :key_endpoint, :iss
 end
