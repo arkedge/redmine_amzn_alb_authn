6d9007445769b020c3c7d5bff597edcb5361edfd
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2023-10-05 13:49:53 +0900

    Update the plugin author

--- init.rb
+++ init.rb
@@ -4,11 +4,11 @@ require_relative 'lib/redmine_amzn_alb_authn'
 
 Redmine::Plugin.register :redmine_amzn_alb_authn do
   name 'Amazon ALB authentication plugin'
-  author 'Takahiro Miyoshi'
+  author 'ArkEdge Space Inc.'
   description 'Use Amazon ALB for user authentication'
   version '0.1.2'
   url 'https://github.com/arkedge/redmine_amzn_alb_auth'
-  author_url 'https://github.com/sankichi92'
+  author_url 'https://arkedgespace.com/'
 end
 
 RedmineAmznAlbAuthn.key_endpoint = ENV.fetch('REDMINE_AMZN_ALB_AUTHN_KEY_ENDPOINT')
